e594d218d6292566908ff2d6a639bbc7274598a1
Michele Calvani <michelecalvani80@gmail.com>
2025-11-07 22:29:58 +0100

    Creazione pagina Cabins con recupero dati da Supabase

---
+++ app/_components/Counter.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>The are in total {users.length} users!</p>
+      <button onClick={() => setCount((c) => c + 1)}>Count: {count}</button>;
+    </div>
+  );
+}
