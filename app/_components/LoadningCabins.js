8a2a2336e520affb83d2b7b96151ab0771c1ad3c
Michele Calvani <michelecalvani80@gmail.com>
2025-11-08 15:58:18 +0200

    Terminata Pagina Dettaglio Cabin + inserimento Metadati dinamici con generateMetadata

--- app/_components/LoadningCabins.js
+++ app/_components/LoadningCabins.js
@@ -4,7 +4,7 @@ export default function LoadingCabins() {
   return (
     <div className="grid place-items-center gap-4 py-16 min-h-[40vh]">
       <Spinner />
-      <p className="text-xl text-primary-200">Loading cabin data...</p>
+      <p className="text-xl text-primary-200">Loading cabins data...</p>
     </div>
   );
 }
